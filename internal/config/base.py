8b5cb90f9d602fdf2958222d2f0054c04d10e106
as.shiryeav <alssval@gmail.com>
2025-05-09 22:48:41 +0300

    feat: add sqlachemy

--- internal/config/base.py
+++ internal/config/base.py
@@ -1,7 +1,10 @@
 from enum import Enum
 from functools import lru_cache
+from multiprocessing import cpu_count
 from pathlib import Path
+from typing import Any
 
+from pydantic import ValidationError, field_validator
 from pydantic_settings import BaseSettings
 
 
@@ -14,12 +17,30 @@ class AppSettings(BaseSettings):
     HOST: str = "0.0.0.0"  # noqa: S104
     PORT: int = 8000
 
-    MODE: StandEnum = StandEnum.LOCAL
+    MODE: StandEnum = StandEnum.PROD
 
-    APP_WORKERS: int | None = None
-    APP_RELOAD: bool = True
+    APP_WORKERS: int = cpu_count()
+    APP_RELOAD: bool = False
 
     APP_CONFIG_LOG: Path = "./config/logging.json"
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_DB: str
+    POSTGRES_URL: str
+
+    @field_validator("POSTGRES_URL")
+    @classmethod
+    def validate_postgres_url(cls, value: str | None = None, values: dict[str, Any] | None = None) -> str:
+        if value:
+            return value
+
+        if not values:
+            raise ValidationError
+
+        return f"postgresql+asyncpg://{values['POSTGRES_USER']}:{values['POSTGRES_PASSWORD']}@{values['POSTGRES_HOST']}:{values['POSTGRES_PORT']}/{values['POSTGRES_DB']}"
 
 
 @lru_cache(maxsize=1)
---
+++ internal/config/models.py
@@ -0,0 +1,32 @@
+from collections.abc import AsyncGenerator
+from functools import lru_cache
+from typing import Any
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
+
+from . import get_config
+
+
+@lru_cache(maxsize=1)
+def get_engine() -> AsyncEngine:
+    return create_async_engine(
+        url=get_config().POSTGRES_URL,
+        echo=True,
+        future=True,
+    )
+
+
+@lru_cache(maxsize=1)
+def get_async_session() -> async_sessionmaker[AsyncSession]:
+    return async_sessionmaker(
+        bind=get_engine(),
+        expire_on_commit=False,
+        class_=AsyncSession,
+    )
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, Any]:
+    async_session_local = get_async_session()
+    async with async_session_local() as session:
+        yield session
