f1db138c7a8919c4460fa86d87aa6ba17b05e79f
as.shiryeav <alssval@gmail.com>
2025-05-19 23:12:42 +0300

    feat: add migrations

---
+++ internal/entities/models/ml.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
+
+from internal.utils.crypto import get_max_length_str_fernet
+
+from .base import SoftModel, UUIDModel
+
+
+class Models(UUIDModel, SoftModel):
+    __tablename__ = "models"
+
+    name = Column(String(length=256), nullable=False)
+    s3_path = Column(String(), nullable=False)
+
+
+class Files(UUIDModel, SoftModel):
+    __tablename__ = "files"
+
+    original_name = Column(String(length=get_max_length_str_fernet(256)), nullable=False)
+    s3_path = Column(String(), nullable=False)
+    type_file = Column(String(length=256), nullable=False)
+    user_id = Column(ForeignKey("user.id"))
+
+
+class Predicts(UUIDModel):
+    __tablename__ = "predicts"
+
+    file_id = Column(ForeignKey("user.id"))
+    model_id = Column(ForeignKey("models.id"))
+    result = Column(Integer(), nullable=False)
+    probability = Column(Float(), nullable=False)
+
+
+class Tasks(UUIDModel, SoftModel):
+    __tablename__ = "tasks"
+
+    file_id = Column(ForeignKey("files.id"))
+    user_id = Column(ForeignKey("user.id"))
+    predict_id = Column(ForeignKey("predicts.id"))
+    message = Column(String(), nullable=True)
+    status = Column(String(256), nullable=False)
